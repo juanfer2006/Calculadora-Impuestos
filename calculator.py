4c35e2eaad27a87926f0d0c283db39be8c604f27
Sebastian <jsuarez337@soyudemedellin.edu.co>
2025-02-18 09:42:36 -0500

    v2

--- calculator.py
+++ calculator.py
@@ -1,3 +1,5 @@
 class Taxes():
-    def calculate(purchase, porcentage):
-        return 1903
+    def calculate(purchase, porcentage, Discount = 0, plastic_bag=0):
+        calculate_final=(purchase - ((Discount/100)*purchase)) * (porcentage/100)
+        return calculate_final
+    
--- test.py
+++ test.py
@@ -21,7 +21,37 @@ class calctest(unittest.TestCase):
         self.assertEqual(round(expected_result), round(result))
 
     def test_normal_3(self):
+        purchase = 1500000
+        porcentage = 19
+        discount = 10
+
+        expected_result = 256500
+        result = Taxes.calculate(purchase, porcentage, discount)
+
+        self.assertEqual(round(expected_result), round(result))
+
+    def test_extraordinary_1(self):
+        purchase = 500000
+        porcentage = 0
+
+        expected_result = 0
+        result = Taxes.calculate(purchase, porcentage)
+
+        self.assertEqual(round(expected_result), round(result))
+
+    def test_extraordinary_1(self):
         pass
+
+    def test_extraordinary_3(self):
+        purchase = 150000
+        porcentage = 19
+
+        expected_result = 178695
+        result = Taxes.calculate(purchase, porcentage)
+
+        self.assertEqual(round(expected_result), round(result))
+
+    
 
         
 if __name__ == '__main__':
